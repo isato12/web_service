fc86528d51d5cd19e3ff23278f42b4735f40cadd
isato12 <isato122@gmail.com>
2022-03-30 19:15:41 -0600

    corrigiendo fallas de conexion

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>web service</title>
+</head>
+<body>
+    <h1>Web Service</h1>
+    <h2>Categorias</h2>
+    <ul>
+        <li><a href="index.php?op=obtenertodos">Obtener todos</a></li>
+        <li><a href="index.php?op=obtenerporid">Obtener por id</a></li>
+        <li><a href="index.php?op=insert">Insertar</a></li>
+        <li><a href="index.php?op=update">Actualizar</a></li>
+        <li><a href="index.php?op=delete">Borrar</a></li>
+    </ul>  
+</body>
+</html>
