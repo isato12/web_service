1434cdf2963f5678b76d3817e450bed26403ed80
isato12 <isato122@gmail.com>
2022-03-30 19:23:19 -0600

    corrigiendo fallas de conexion 2

--- index.php
+++ index.php
@@ -5,10 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>web service</title>
+    <link rel="stylesheet" href="style.css">
 </head>
-<body>
-    <h1>Web Service</h1>
-    <h2>Categorias</h2>
+<body class="container">
+    <h1 class="titulo">Web Service</h1>
+    <h2 class="subtitulo">Categorias</h2>
     <ul>
         <li><a href="index.php?op=obtenertodos">Obtener todos</a></li>
         <li><a href="index.php?op=obtenerporid">Obtener por id</a></li>
